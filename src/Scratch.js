b34d92bd17d7c3cda42bba9e87e403cc105843f4
Joezilla2014 <63375064+Joezilla2014@users.noreply.github.com>
2020-06-07 11:38:31 -0400

    Update Scratch.js

--- src/Scratch.js
+++ src/Scratch.js
@@ -74,7 +74,7 @@ import './watchers/ListWatcher.js';
 
 class Scratch extends Sprite {
 	// Version
-	static var app = new Scratch(); // static reference to the app, used for debugging
+	var app = new Scratch(); // static reference to the app, used for debugging
 
 	// Display modes
 	var hostProtocol = 'http';
